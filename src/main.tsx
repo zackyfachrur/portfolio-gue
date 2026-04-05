54642b232aa13fda13d0f32bad8c8f6f77ab0607
zackyfachrur <mochzackyaja@gmail.com>
2026-04-05 19:08:18 +0700

    feat: add some design to HomeSection.tsx & ProjectSection.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import './index.css'
 import 'remixicon/fonts/remixicon.css';
 import App from './App.tsx'
 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
---
+++ src/types/swiper-css.d.ts
@@ -0,0 +1,19 @@
+declare module 'swiper/css' {
+  const content: string;
+  export default content;
+}
+
+declare module 'swiper/css/navigation' {
+  const content: string;
+  export default content;
+}
+
+declare module 'swiper/css/pagination' {
+  const content: string;
+  export default content;
+}
+
+declare module 'swiper/css/scrollbar' {
+  const content: string;
+  export default content;
+}
